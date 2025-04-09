d3652cb7033283c759bbe8759e5a526eb18a9129
Juan2004Pablo <juanpapabon@gmail.com>
2025-04-09 13:48:49 -0500

    refactor: improve the design and user experience of views

--- src/main/java/com/example/demo_2/Controllers/CategoryController.java
+++ src/main/java/com/example/demo_2/Controllers/CategoryController.java
@@ -2,7 +2,11 @@ package com.example.demo_2.Controllers;
 
 
 import com.example.demo_2.Models.Entities.Category;
+import com.example.demo_2.Models.Entities.Detail;
 import com.example.demo_2.Services.CategoryService;
+import com.example.demo_2.Services.DetailService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/categories")
 public class CategoryController {
+    @Autowired
+    private DetailService cartService;
 
     private final CategoryService categoryService;
 
@@ -19,7 +25,13 @@ public class CategoryController {
 
     @GetMapping
     public String listCategories(Model model) {
+        Long cartId = cartService.getLatestActiveDetailId();
+        Detail cart = cartService.getCartDetailById(cartId);
+        Long productCount = cartService.countProductsInDetail(cart.getIdDetail());
+
         model.addAttribute("categories", categoryService.findAll());
+        model.addAttribute("productCount", productCount);
+
         return "categories/list";
     }
     // Muestra el formulario de creación
